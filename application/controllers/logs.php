2f708f10386d4ace4f76f42f543579c67ca7b21a
sultanspermadi <sultans.permadi@gmail.com>
2014-06-26 09:12:21 -0700

    report-log
    :)

---
+++ application/controllers/logs.php
@@ -0,0 +1,24 @@
+<?php
+
+class Logs extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model(array('default_model'));
+	}
+
+	public function Index()
+	{
+		$view  = 'report/log/default';
+		$data  = $this->log->get(10);
+		$structure = array("user_id"=>"User", "description"=>"Desksripsi", "datetime"=>"Date Time");
+		$parse = array(
+			"view" => $view,
+			"data" => $data,
+			"structure" => $structure
+			);
+		$this->load->view('template',$parse);
+	}
+
+}
